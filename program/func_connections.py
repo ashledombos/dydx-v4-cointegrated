f652a4bdfc3ff338ebf5f3db4ce2355a3c7c1773
ashledombos <rj@hodo.fr>
2024-05-17 00:41:49 +0200

    Update func_connections.py

--- program/func_connections.py
+++ program/func_connections.py
@@ -1,32 +1,50 @@
-from v4_client_py.clients import IndexerClient, ValidatorClient
-from v4_client_py.clients.constants import Network
-from constants import DYDX_TEST_MNEMONIC
+from decouple import config
+from dydx4 import Client  # Assurez-vous d'utiliser la bibliothèque dydx4
+from constants import (
+    HOST,
+    DYDX_API_KEY,
+    DYDX_API_SECRET,
+    DYDX_API_PASSPHRASE,
+)
 
-# Connect to DYDX v4 API
 def connect_dydx():
     """
-    Crée et retourne un client dYdX connecté à l'aide de la v4 API.
-    Utilise la configuration du réseau spécifiée pour se connecter aux endpoints nécessaires.
+    Connexion à DYDX et retour du client.
+
+    Crée une connexion client à l'API DYDX en utilisant les informations d'identification fournies. 
+    Affiche et renvoie les informations du compte DYDX si la connexion est réussie.
+
+    Returns:
+        client (Client): Instance du client DYDX connecté.
     """
 
-    # Configuration du réseau
-    network_config = Network.config_network()
-    indexer_client = IndexerClient(
-        config=network_config.indexer_config,
-    )
-    validator_client = ValidatorClient(
-        config=network_config.validator_config,
-    )
-
-    # Vérification de la connexion
     try:
-        # Pour des vérifications supplémentaires, on pourrait implémenter des appels à des endpoints spécifiques,
-        # par exemple, vérifier le statut de l'API ou des informations de compte spécifiques.
-        print("Connection Successful to Indexer and Validator Clients")
-        
-        # Retourne les clients connectés
-        return indexer_client, validator_client
-    except Exception as e:
-        print("Failed to connect to dYdX v4 clients:", e)
-        raise
+        # Création de la connexion client
+        client = Client(
+            host=HOST,
+            api_key_credentials={
+                "key": DYDX_API_KEY,
+                "secret": DYDX_API_SECRET,
+                "passphrase": DYDX_API_PASSPHRASE,
+            }
+        )
 
+        # Confirmation de la connexion du client
+        account = client.private.get_account()
+        account_id = account['account']['id']
+        balance = account['account']['balance']
+
+        print("Connexion réussie")
+        print("ID du compte : ", account_id)
+        print("Solde du compte : ", balance)
+
+        # Retourne le client connecté
+        return client
+
+    except Exception as e:
+        print(f"Erreur lors de la connexion à DYDX : {e}")
+        return None
+
+# Appel de la fonction de connexion pour tester
+if __name__ == "__main__":
+    connect_dydx()
